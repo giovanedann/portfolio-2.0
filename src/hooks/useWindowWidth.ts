ec1179bd5df739165593de360ddaf06bd78de7d0
Giovane <giovanedaniell@gmail.com>
2024-05-22 10:31:17 -0300

    feat: add checking to avoid server build errors

--- src/hooks/useWindowWidth.ts
+++ src/hooks/useWindowWidth.ts
@@ -1,8 +1,10 @@
 'use client'
 import { useCallback, useEffect, useState } from 'react';
 
+const isClientSide = typeof window !== 'undefined'
+
 export function useWindowWidth() {
-  const [width, setWidth] = useState(window.innerWidth)
+  const [width, setWidth] = useState(isClientSide ? window.innerWidth : 1080)
 
   const handleResize = useCallback(() => {
     setWidth(window.innerWidth)
